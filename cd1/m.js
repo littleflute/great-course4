44bec523442d01895778211889c1a5df7eeba974
littleflute <13699175164@sina.cn>
2017-06-15 15:55:49 -0500

    Add files via upload

---
+++ cd1/m.js
@@ -0,0 +1,14 @@
+var f = [];				
+f[0]	= "v0.0.9";			
+f[1]	= "01 Lecture01.mp3";		
+f[2]	= "02 Lecture02.mp3";		
+f[3]	= "03 Lecture03.mp3";		
+f[4]	= "04 Lecture04.mp3";		
+f[5]	= "05 Lecture05.mp3";		
+f[6]	= "06 Lecture06.mp3";		
+f[7]	= "07 Lecture07.mp3";		
+f[8]	= "08 Lecture08.mp3";		
+f[9]	= "09 Lecture09.mp3";		
+f[10]	= "10 Lecture10.mp3";		
+f[11]	= "11 Lecture11.mp3";		
+f[12]	= "12 Lecture12.mp3";		
